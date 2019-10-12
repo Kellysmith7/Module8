770ed7fc22e4c6c8b65b51f3ea45bcb6555da0f0
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-12 15:17:02 -0500

    Passed True and False

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,17 +1,22 @@
-# Create a file test_set_membership.py, a unit test in test.
-# Create a file set_membership.py in more_fun_with_collections.
-# In set_membership.py
-def in_set():
-    #kelly_set = {'red', 'blue', 'green', 'yellow'}
-    #if ('red' in kelly_set)
-        #print ('True')
-    pass
-#This will eventually accept a set and return a boolean value stating if the element is in the set.
-# In test_set_membership.py,
-# Write a test test_in_set_true
-# assertion for an item in the set (expect True)
-# Write a test test_in_set_true
-# One assertion for an item not in the set (expect False)
-# Run the test, see it fail, commit to github
-# Write the in_set function.
-# Run the test, see it pass, commit to github.
+"""
+Program: set_membership.py
+Author: Kelly Smith
+Last day updated: 10-12-19
+
+Program to determine if a color is in the set
+
+:param kelly_set: set of colors
+:return True or False if a color is in the set
+"""
+
+
+def in_set(x):
+    kelly_set = {'red', 'blue', 'green', 'yellow'}
+    if x in kelly_set:
+        return 'True'
+    else:
+        return 'False'
+
+
+if __name__ == '__main__':
+    print(in_set('red'))
--- test_more_fun_with_collections/test_set_membership.py
+++ test_more_fun_with_collections/test_set_membership.py
@@ -4,10 +4,10 @@ from more_fun_with_collections import set_membership
 
 class MyTestCase(unittest.TestCase):
     def test_in_case_set_true(self):
-        self.assertEqual(True, set_membership.in_set())
+        self.assertEqual('True', set_membership.in_set('red'))
 
     def test_in_case_set_false(self):
-        self.assertEqual(False, set_membership.in_set())
+        self.assertEqual('False',set_membership.in_set('orange'))
 
 
 if __name__ == '__main__':
