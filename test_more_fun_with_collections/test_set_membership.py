0e3329be76d9ae86b87105cf36bb7ec506e9f073
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-12 15:10:32 -0500

    Failed True test and false

--- test_more_fun_with_collections/test_set_membership.py
+++ test_more_fun_with_collections/test_set_membership.py
@@ -6,6 +6,9 @@ class MyTestCase(unittest.TestCase):
     def test_in_case_set_true(self):
         self.assertEqual(True, set_membership.in_set())
 
+    def test_in_case_set_false(self):
+        self.assertEqual(False, set_membership.in_set())
+
 
 if __name__ == '__main__':
     unittest.main()
