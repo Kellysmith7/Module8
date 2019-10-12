32980d87d4048bb6988f17f911cce99ce7a9159b
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-12 15:25:50 -0500

    Dictionary Failed True and False

---
+++ more_fun_with_collections/dict_membership.py
@@ -0,0 +1,17 @@
+"""
+Program: dict_membership.py
+Author: Kelly Smith
+Last day updated: 10-12-19
+
+Program to find an elemenet in a dictonary
+:param
+:return
+"""
+
+
+def in_dict():
+    pass
+#This will eventually accept a set and return a boolean value stating if the element is in the set.
+
+
+
---
+++ test_more_fun_with_collections/test_dict_membership.py
@@ -0,0 +1,14 @@
+import unittest
+from more_fun_with_collections import dict_membership
+
+
+class MyTestCase(unittest.TestCase):
+    def test_in_dict_true(self):
+        self.assertEqual('True', dict_membership.in_dict())
+
+    def test_in_dict_false(self):
+        self.assertEqual('False', dict_membership.in_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
