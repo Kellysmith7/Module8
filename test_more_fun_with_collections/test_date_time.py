c2c9b43f581665a5a6bd0b21a8f1d43b51b4ba5a
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-13 10:21:42 -0500

    Pass Half Birthday

--- test_more_fun_with_collections/test_date_time.py
+++ test_more_fun_with_collections/test_date_time.py
@@ -1,10 +1,11 @@
 import unittest
 from more_fun_with_collections import date_time
+from datetime import date
 
 
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
-        self.assertEqual('2019 12 5', date_time.half_birthday(1981, 6, 5))
+        self.assertEqual((2019, 12, 5), date_time.half_birthday(date(1981, 6, 5)))
 
 
 if __name__ == '__main__':
