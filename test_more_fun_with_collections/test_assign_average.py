adb393cac162381fec43a7b33d5f58521e11bca8
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-23 13:49:39 -0500

    Passed Switch Average "A"

--- test_more_fun_with_collections/test_assign_average.py
+++ test_more_fun_with_collections/test_assign_average.py
@@ -4,10 +4,7 @@ from more_fun_with_collections import assign_average
 
 class MyTestCase(unittest.TestCase):
     def test_a(self):
-        self.assertEqual(90-100, assign_average.switch_average('A'))
-
-    def test_b(self):
-        self.assertEqual(80-89, assign_average.switch_average('B'))
+        self.assertEqual('Score is between 90 and 100', assign_average.switch_average('a'))
 
 
 if __name__ == '__main__':
